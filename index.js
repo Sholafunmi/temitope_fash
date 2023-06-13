9d1f3c8b50d24f7ad97e0e7f330b5ff8c8f2bbe2
Sholafunmi <sholafunmiadewara@gmail.com>
2023-06-13 10:57:32 +0100

    sideNav

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const showSidebar = document.querySelector(".hamburger");
 const sideMenuBar = document.querySelector(".side-menu-bar");
 const closeSideBar = document.querySelector(".close-menu-bar");
+const sideMenuItems = document.querySelector(".side-menu");
 let bar = [];
 
 
@@ -11,5 +12,9 @@ sideMenuBar.style.transform = "translate(0%)"
 
 // close side bar 
 closeSideBar.addEventListener("click", function(){
+    sideMenuBar.style.transform = "translate(100%)"
+})
+
+sideMenuItems.addEventListener("click", function(){
     sideMenuBar.style.transform = "translate(100%)"
 })
