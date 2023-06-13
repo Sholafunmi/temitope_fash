51ba19fa3c75bcc8beb25576420d9420140770e7
Sholafunmi <sholafunmiadewara@gmail.com>
2023-06-13 11:46:24 +0100

    skits

--- index.js
+++ index.js
@@ -18,3 +18,24 @@ closeSideBar.addEventListener("click", function(){
 sideMenuItems.addEventListener("click", function(){
     sideMenuBar.style.transform = "translate(100%)"
 })
+
+
+
+
+// biography read more
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
